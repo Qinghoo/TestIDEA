87a511c806f9a4748b5e81b47c5d671720999ae3
liuqinghao1998 <liuqinghao1998@163.com>
2019-04-07 11:43:16 +0800

    Initial commit

---
+++ src/com/tsxy/hee/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tsxy.hee;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Student student=new Student();
+        student.setAge(45);
+        student.setName("Jack");
+        System.out.println(student);
+    }
+}
